a364484c2b307250868a5bc0d4aa1ae1bbf91537
cirowagner <64237190+cirowagner@users.noreply.github.com>
2020-07-03 16:07:29 -0500

    EXAMEN UNIDAD 2

--- ExamenCMM/src/main/java/pe/edu/upeu/cmm/App.java
+++ ExamenCMM/src/main/java/pe/edu/upeu/cmm/App.java
@@ -1,11 +1,15 @@
-//======================================MENU DE OPCIONES========================================00
+//======================================MENU DE OPCIONES====================================================================================00
 package pe.edu.upeu.cmm;
 
 import java.io.IOException;
 import pe.edu.upeu.cmm.examen.*;
 import pe.edu.upeu.cmm.utils.*;
 
-public class App{	
+public class App{
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
 
     public static void main(String[] args) throws IOException {
         try {
@@ -15,33 +19,43 @@ public class App{
             char opcion='S'; //S=SI, N=NO
             String NumAlgoritm="01";
 
-            do{            
+            do{          
+				System.out.println(ANSI_PURPLE+"---¡HOLA!---"+ANSI_RESET);
                 NumAlgoritm = teclado.leer("",
-                "Ingrese el numero de Algoritmo que desee probar: \n01=Impuestos\n02= Tablas_Muiltipli\n03= Numeros_Perfectos \n04= ExponenteX");
+                ANSI_GREEN+"Ingrese el numero de Algoritmo que desee probar: \n"+
+				"01= IMPUESTOS_POR_VEHICULOS\n"+
+				"02= TABLAS_DE_MULTIPLICACION\n"+
+				"03= NUMEROS_PERFECTOS\n"+
+				"04= ExponenteX"+ANSI_RESET);
 				
                 switch(NumAlgoritm){
                 case "01":
-				int NumVehiculos = teclado.leer(0,"Ingrese el numero de vehiculos");
-				obj.Impuestos(NumVehiculos);
+				int NumVehiculos = teclado.leer(0,ANSI_RED+"Ingrese el numero de vehiculos"+ANSI_RESET);
+				obj.IMPUESTOS_POR_VEHICULOS(NumVehiculos);
 				break;
 				case "02":
-                int Tablas = teclado.leer(0,"Introduce un numero de tablas a multiplicar: ");
-				obj.Tablas_Multipli(Tablas);
+                int Tablas = teclado.leer(0,ANSI_RED+"Introduce un numero de tablas a multiplicar"+ANSI_RESET);
+				obj.TABLAS_DE_MULTIPLICACION(Tablas);
 				break;
 				case "03": 
-				obj.Numeros_Perfectos();
+				obj.NUMEROS_PERFECTOS();
 				break;
 				case "04":
-				int n = teclado.leer(0,"Ingrese la base");
-				int x = teclado.leer(0,"Ingrese el exponente");
-				System.out.println("El resultado es: "+obj.ExponenteX(n,x));
+				int num = teclado.leer(0,ANSI_RED+"Ingrese la base"+ANSI_RESET);
+				int exp = teclado.leer(0,ANSI_RED+"Ingrese el exponente"+ANSI_RESET);
+				System.out.println(ANSI_RED+"El resultado es: "+obj.ExponenteX(num,exp)+ANSI_RESET);
 				break;
                 default: System.out.println("La opcion No existe!!"); break;
-                }            
-                opcion=teclado.leer(' ', "Desea Probar mas Algoritmos? S=SI, N=NO");
+                }   
+                System.out.println();
+                opcion=teclado.leer(' ', "|---¿Desea Probar mas Algoritmos?---> S=SI, N=NO");
             }while(opcion=='S' || opcion=='s');   
         } catch (Exception er) {
             System.out.println(er.getMessage());
         }
+		System.out.println();
+		System.out.println(ANSI_PURPLE+"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"+ANSI_RESET);
+		System.out.println(ANSI_PURPLE+"zzz    <---ESO ES TODO¡GRACIAS!--->       zzz"+ANSI_RESET);
+		System.out.println(ANSI_PURPLE+"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"+ANSI_RESET);
     }
 }
--- ExamenCMM/src/main/java/pe/edu/upeu/cmm/examen/ResolucionExamen.java
+++ ExamenCMM/src/main/java/pe/edu/upeu/cmm/examen/ResolucionExamen.java
@@ -5,11 +5,12 @@ import pe.edu.upeu.cmm.utils.LeerTeclado;
 public class ResolucionExamen{
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_RED = "\u001B[31m";
 	
 	LeerTeclado leerT = new LeerTeclado();
 //1 ===========================================================================================================================================
-	public void Impuestos(int NumVehiculos){
-	System.out.println("---IMPUESTOS---"); 
+	public void IMPUESTOS_POR_VEHICULOS(int NumVehiculos){
+	System.out.println(ANSI_RED+"---IMPUESTOS---"+ANSI_RESET); 
     System.out.println(); 	
 	
 	int CostoVehiculo = 0, Categoria = 0;
@@ -20,40 +21,47 @@ public class ResolucionExamen{
     while(x <= NumVehiculos){
 	
 		Categoria = leerT.leer(0,"Ingrese la categoria del vehiculo numero "+x);
+		System.out.println();
 		CostoVehiculo = leerT.leer(0,"Ingrese el costo de vehiculo numero "+x);
+		System.out.println();
 		
 		if(Categoria == 1){
-			
-			Impuesto1 = CostoVehiculo*0.10;
-			System.out.println("El costo del vehiculo numero "+x+" es de "+CostoVehiculo+" y el impuesto a pagar por este es de: "+Impuesto1);
-			
+			Impuesto1 = CostoVehiculo * 0.10;
+			System.out.println(ANSI_GREEN+"El costo del vehiculo numero "+x+" es de: S/"+CostoVehiculo+" Soles y el impuesto a pagar por este es de: S/"+Impuesto1+" Soles"+ANSI_RESET);
+			System.out.println("------------------------------------------------------------------------------------------------------");
+		
 		}else if(Categoria == 2){
 			Impuesto2 = CostoVehiculo * 0.07;
-			System.out.println("El costo del vehiculo numero "+x+" es de "+CostoVehiculo+" y el impuesto a pagar por este es de: "+Impuesto2);
+			System.out.println(ANSI_GREEN+"El costo del vehiculo numero "+x+" es de: S/"+CostoVehiculo+" Soles y el impuesto a pagar por este es de: S/"+Impuesto2+" Soles"+ANSI_RESET);
+		    System.out.println("------------------------------------------------------------------------------------------------------");
 		
 		}else{
 			Impuesto3 = CostoVehiculo * 0.05;
-			System.out.println("El costo del vehiculo numero "+x+" es de "+CostoVehiculo+" y el impuesto a pagar por este es de: "+Impuesto3);
+			System.out.println(ANSI_GREEN+"El costo del vehiculo numero "+x+" es de: S/"+CostoVehiculo+" Soles y el impuesto a pagar por este es de: S/"+Impuesto3+" Soles"+ANSI_RESET);
+			System.out.println("------------------------------------------------------------------------------------------------------");
         }
          x++;
   }
+    System.out.println();
     ImpuestoTotal = Impuesto1 + Impuesto2 + Impuesto3;
-	System.out.println("El impuesto total a pagar es de: "+ImpuestoTotal);
+	System.out.println("El impuesto total a pagar es de: S/"+ImpuestoTotal+" Soles");
+	System.out.println("=======================================================================");
+	System.out.println(ANSI_RED+"---END---"+ANSI_RESET);
+    System.out.println(); 
 }
 
 //2 =============================================================================================================================================
 
-    public void Tablas_Multipli(int Tablas){
-	
+    public void TABLAS_DE_MULTIPLICACION(int Tablas){
+		
 	 int i;
-		System.out.println();
 		
 		int x = 0, n = 1;
 		while(x < Tablas){
 	    System.out.println("-------------");
 		System.out.println(); 
         System.out.println("============");		
-        System.out.println(ANSI_GREEN+"Tabla del " + n+""+ANSI_RESET);
+        System.out.println(ANSI_RED+"Tabla del " + n+""+ANSI_RESET);
 		System.out.println("============");
 		
         for(i = 1; i<=10; i++){
@@ -63,11 +71,15 @@ public class ResolucionExamen{
 		n++;
 		x++;
 	   }
+	   System.out.println(ANSI_RED+"---END---"+ANSI_RESET);
+	   System.out.println(); 
 	 }
-//3============================================================================================================================================
+//3==============================================================================================================================================
 
-    public void Numeros_Perfectos(){
-
+    public void NUMEROS_PERFECTOS(){
+	System.out.println(ANSI_RED+"---LOS CUATRO PRIMEROS NUMEROS PERFECTOS---"+ANSI_RESET); 
+    System.out.println(); 
+	
 	double NumPerfect = 0;
 	
 	int x = 1, n = 2;
@@ -75,38 +87,43 @@ public class ResolucionExamen{
 	while(x <= 6){
 	if(n == 2){
 	NumPerfect = Math.pow(2,n-1)*(Math.pow(2,n)-1);
-	System.out.println("El primer numero perfecto es: "+NumPerfect);
+	System.out.println(ANSI_GREEN+"El primer numero perfecto es: "+NumPerfect+" y sus numeros enteros divisores son 1,2,3"+ANSI_RESET);
+	System.out.println("-------------------------------------------------------------------------------------"); 
 	
 	}else if(n == 3){
 	NumPerfect = Math.pow(2,n-1)*(Math.pow(2,n)-1);
-	System.out.println("El segundo numero perfecto es: "+NumPerfect);
+	System.out.println(ANSI_GREEN+"El segundo numero perfecto es: "+NumPerfect+" y sus numeros enteros divisores son 1,2,4,7,14"+ANSI_RESET);
+	System.out.println("-----------------------------------------------------------------------------------------"); 
 	
 	}else if(n == 5){
 	NumPerfect = Math.pow(2,n-1)*(Math.pow(2,n)-1);
-	System.out.println("El tercer numero perfecto es: "+NumPerfect);
+	System.out.println(ANSI_GREEN+"El tercer numero perfecto es: "+NumPerfect+" y sus numeros enteros divisores son 1,2,4,8,16,31,62,124,248"+ANSI_RESET);
+	System.out.println("----------------------------------------------------------------------------------------------------"); 
 	
 	}else if(n == 7){
 	NumPerfect = Math.pow(2,n-1)*(Math.pow(2,n)-1);
-	System.out.println("El cuarto numero perfecto es: "+NumPerfect);
+	System.out.println(ANSI_GREEN+"El cuarto numero perfecto es: "+NumPerfect+" y sus numeros enteros divisores son 1,2,4,8,16,32,64,127,254,508,1016,2032,4064"+ANSI_RESET);
+	System.out.println("----------------------------------------------------------------------------------------------------------------------"); 
 	}
 	x++;
 	n++;
-	}	
+	}
+  System.out.println(ANSI_RED+"---END---"+ANSI_RESET);
+  System.out.println(); 	
   }
 
 //4============================================================================================================================================
 
-public int ExponenteX(int n, int x){
-	
+public int ExponenteX(int num, int exp){ 
 	int Resultado = 0;
 	
-    if (x <= 0){
+    if (exp <= 0){
         Resultado = 1;
 	}else{
-		Resultado = n*ExponenteX(n,(x-1));
+		Resultado = num*ExponenteX(num,(exp-1));
 	}
 	return Resultado;
-  }
+  } 
 //Fin Resolucion de Examen=====================================================================================================================
  }
 
